34dfd310a18d6433ec327df77bc18b7ed508f769
Alradenn Ibno <alradennibno@gmail.com>
2025-12-30 11:36:35 +0800

    chore(release): prepare v1.0.0 for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import express from 'express'
+import path from 'path'
 import { dbConnect } from './config/dbConnect.js'
 import cookieParser from 'cookie-parser'
 
@@ -12,6 +13,8 @@ import protectRoute from './middleware/authenticate.js'
 
 const app = express()
 
+const __dirname = path.resolve()
+
 const PORT = process.env.PORT || 5300
 
 app.use(express.json())
@@ -21,6 +24,14 @@ app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/movie', protectRoute, movieRouter)
 app.use('/api/v1/tv', protectRoute, tvRouter)
 app.use('/api/v1/search', protectRoute, searchRoute)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
